9232f33489d0039e97d109d7a2483a2f716f52d2
Pavan kumar Kulkarni <pavankumar.kulkarni@gmail.com>
2021-04-08 20:43:49 -0400

    logging

--- src/index.js
+++ src/index.js
@@ -6,12 +6,14 @@ import { ApolloClient } from "@apollo/client/core";
 import { InMemoryCache } from "@apollo/client/cache";
 import { ApolloProvider } from "@apollo/client/react";
 
-const graphql = process.env.graphql || "http://localhost:5000/graphql";
+const graphql_uri = process.env.graphql || "http://localhost:5000/graphql";
 
 const client = new ApolloClient({
-  uri: graphql,
+  uri: graphql_uri,
   cache: new InMemoryCache(),
 });
+
+console.log(graphql_uri);
 
 ReactDOM.render(
   <React.StrictMode>
